deaad913748d65a236994aa49eb815d3032237a7
serendipity <40058000+serendipityh@users.noreply.github.com>
2019-08-29 10:38:48 +0800

    fix

---
+++ day001/day001.py
@@ -0,0 +1,14 @@
+'''
+print("hello,world")
+
+print("The quick brown fox","jumps over","the lazy dog")
+
+print('100+200=',100+200)
+
+name=input('please enter your name: ');
+
+print('hello',name)
+
+'''
+
+print('1024*768 =', 1024*768);
