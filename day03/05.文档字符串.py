878e17bd5b59d6506d806e37d37c69c4917c0c18
serendipityH <2274734747@qq.com>
2019-12-11 22:36:30 +0800

    文档字符串

---
+++ day03/05.文档字符串.py
@@ -0,0 +1,14 @@
+#help()是python的函数内置对象
+
+#help(print) 获取print()函数的使用说明
+
+def fn(a:int,b:bool,c) ->int:
+    '''
+    :param a:
+    :param b:
+    :param c:
+    :return:
+    '''
+    return
+
+help(fn)
